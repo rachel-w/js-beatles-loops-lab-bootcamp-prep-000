113cae124da2445704552235807a3b9934f077b6
Rachel Wey <rachelwey96@outlook.com>
2018-10-27 08:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
+  var beatles = []
   for (var i = 0; i < musicians.length; i++) {
-  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  	beatles.push(musicians[i] + ' plays ' + instruments[i])
   }
-  return array;
+  return beatles
 }
 
 function johnLennonFacts(facts){
