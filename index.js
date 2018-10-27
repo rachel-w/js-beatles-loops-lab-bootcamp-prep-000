cf365d14f40b20e71458bcd4f385b0811fe72160
Rachel Wey <rachelwey96@outlook.com>
2018-10-27 08:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var beatles = []
+  for(let i = 0; i < array.length; i++){
+    array.push(`${musicians[0]}
+     plays ${instruments[0]}`)
+  }
+  return beatles
+}
+
+function johnLennonFacts(array){
+  var johnArray = []
+  var len = array.length
+  While(len > 0){
+    console.log(len)
+    johnArray.unshift(array[len-1]+"!!!")
+    len--
+  }
+  return johnArray
+}
+
+function iLoveTheBeatles(number){
+  var loveArray = []
+  do{
+    
+  }while(let i = 0; i < 15; i++)
+}
